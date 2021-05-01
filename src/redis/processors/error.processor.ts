b759774807a77376ca01fcc34575ed239597829e
Daniel Alves <daniel.alves@portus.tech>
2021-05-01 00:27:57 -0300

    feature/error_queue : added error queue handling

---
+++ src/redis/processors/error.processor.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { InjectQueue, OnQueueFailed, Process, Processor } from "@nestjs/bull";
+
+import { Job, Queue } from "bull";
+
+import { CqrsQueueProcessors } from "../../enums";
+import { EventBusService, QueueRegistryService } from "../../services";
+
+@Processor(CqrsQueueProcessors.ERROR_QUEUE)
+export class ErrorQueueProcessor {
+  constructor(
+    @InjectQueue(CqrsQueueProcessors.ERROR_QUEUE)
+    private readonly queue: Queue,
+    private readonly queueRegistryService: QueueRegistryService,
+    private readonly eventBus: EventBusService
+  ) {
+    this.eventBus.observable().subscribe({
+      next: this.onMessageDispatch.bind(this),
+    });
+  }
+
+  @OnQueueFailed()
+  onError(job: Job<any>, error: any) {
+    console.log("error", error, job.data);
+  }
+
+  @Process(CqrsQueueProcessors.ERROR_QUEUE)
+  process(job: Job<any>) {
+    this.queueRegistryService.handle(CqrsQueueProcessors.ERROR_QUEUE, job.data);
+  }
+
+  async onMessageDispatch(message: any) {
+    await this.queue.add(CqrsQueueProcessors.ERROR_QUEUE, message);
+  }
+}
---
+++ src/services/error-bus.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+import { CqrsQueueProcessors } from "../enums";
+import { AbstractMessage } from "../types";
+
+import { QueueRegistryService } from "./queue-registry.service";
+import { AbstractBusService } from "./abstract-bus.service";
+
+@Injectable()
+export class ErrorBusService extends AbstractBusService<AbstractMessage> {
+  constructor(queueRegistryService: QueueRegistryService) {
+    super(CqrsQueueProcessors.ERROR_QUEUE, queueRegistryService);
+  }
+}
