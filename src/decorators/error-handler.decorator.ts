c6b3f8c1de32f7d240a489e263d2ef1e93e0f90a
Daniel Alves <daniel.alves@portus.tech>
2021-05-01 00:33:16 -0300

    feature/error_queue : added error decorators

---
+++ src/decorators/error-handler.decorator.ts
@@ -0,0 +1,16 @@
+import { CqrsQueueProcessors, Metatypes } from "../enums";
+import { AbstractMessage } from "../types";
+
+import { MessageHandler } from "./message-handler.decorator";
+
+export function ErrorHandler(
+  messageType: typeof AbstractMessage
+): ClassDecorator {
+  return (target: any) => {
+    return MessageHandler(
+      CqrsQueueProcessors.ERROR_QUEUE,
+      Metatypes.ErrorHandler,
+      messageType
+    )(target);
+  };
+}
